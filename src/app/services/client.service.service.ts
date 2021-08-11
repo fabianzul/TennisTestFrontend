77afa816ade0520941e8e6d34bd5500f41fa4552
Carlos <czuluaga@omnixcorp.com>
2021-08-11 09:17:40 -0500

    fix bug in REST_API_SERVER url

--- src/app/services/client.service.service.ts
+++ src/app/services/client.service.service.ts
@@ -8,7 +8,7 @@ import { client } from '../models/client';
   providedIn: 'root'
 })
 export class ClientService {
-  private REST_API_SERVER = "http://http://tennistest.us-east-1.elasticbeanstalk.com/clients/";
+  private REST_API_SERVER = "http://tennistest.us-east-1.elasticbeanstalk.com/clients/";
 
   constructor(private httpClient: HttpClient) { }
 
